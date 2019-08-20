1bdcc7d2d1c6f29d028d46fd454a18f120ce8114
overbejt <overbejt@miamioh.edu>
2019-08-19 21:47:01 -0400

    initial commit.  Boiler plate code complete

---
+++ darts.py
@@ -0,0 +1,36 @@
+import pygame
+
+print("so far so good!")
+
+# defining a main function
+
+
+def main():
+
+    # initializing a pygame module
+    pygame.init()
+    # setting the caption
+    pygame.display.set_caption("My version of darts game")
+
+    # creating a surface on the screen that has a size of 240 x 180
+    screen = pygame.display.set_mode((240, 180))
+
+    # declaring and initializing to controll when the game is running
+    running = True
+
+    # game loop
+    while(running):
+        # handling game events
+        for event in pygame.event.get():
+            # Only handling quit events
+            if(event.type == pygame.QUIT):
+                # flip "running"
+                running = False
+
+
+# This will make the game run only if it is the main program
+# If it is imported, then nothing will happen
+if __name__ == "__main__":
+    # call the main function
+    main()
+    print("Auf weider sehen!")
